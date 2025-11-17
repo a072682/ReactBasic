7211a47b7a4b5bcdf527245a2041c74edda2d0e1
CubeAndy <a072682@gmail.com>
2025-11-17 14:18:01 +0800

    自訂卷軸範例更新

---
+++ src/components/common/常用元件範例/視窗自訂義卷軸範例/Scrollbar.jsx
@@ -0,0 +1,18 @@
+
+import './_Scrollbar.scss';
+
+function Scrollbar (){
+
+    return(
+        <>
+         <div className='box'>
+            <p className='textSet'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil itaque sit ullam. Quasi, doloribus commodi. Possimus laboriosam iure, numquam exercitationem et unde consectetur sunt, quibusdam repellendus quas magnam voluptate placeat!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati alias nostrum, exercitationem pariatur provident ab aliquid ducimus assumenda laboriosam, nemo eligendi non voluptates accusantium consequatur voluptatum saepe perspiciatis minima enim.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore dolore cupiditate animi quod, obcaecati, sint esse sapiente fuga iusto vel voluptate recusandae laboriosam maxime, debitis dignissimos. Fuga dolorum sequi nobis?
+            </p>
+         </div>
+        </>
+    )
+}
+export default Scrollbar;
